42e32772a1b1e3226aa07ab2db38cea59cb4b813
soldierzeus <hjsnam@naver.com>
2023-01-13 09:03:31 +0900

    upload

--- my_languages/chat.py
+++ my_languages/chat.py
@@ -1,15 +1,21 @@
+#  파이썬에 적용할 모듈을 이용합니다.
 import os
 import openai
 from playsound import playsound
 
+# 자신이 OPENAI 계정에 있는 API-KEY를 복사하여서 여기에다가 붙여 놓습니다.
 openai.api_key = input("Enter your API-KEY : ")
 
+# AI와 Human의 주체를 생성합니다.
 start_sequence = "\nAI:"
 chat_question = "\nHuman: "
 
+# OPENAI중 CHAT 봇 중 text-davinci-003이 채팅의 머신러닝을 가진 봇이므로 이를 이용하여 AI 봇을 만듭니다.
 firstchat_response_sentence = "The following is a conversation with an listening music AI assistant. The assistant is helpful, creative, clever, and very friendly.\n"
 print(firstchat_response_sentence)
 my_prompt = firstchat_response_sentence
+
+# 마지막으로 AI 봇이 "Enjoy"문장이 포함되면 이에 따라 자동적으로 음원 다운로드가 되며, 재생됩니다.
 while True: 
   chat_answer = input(f"{chat_question}")
   my_prompt = my_prompt + chat_question + chat_answer 
@@ -18,6 +24,8 @@ while True:
   my_prompt += response["choices"][0]["text"]
   if "Enjoy" in my_prompt:
     playsound(f"my_languages\\music_resources\\{chat_answer}.mp3")
+    
+    # 음원 재생이 마치면 이 프로그램은 종료됩니다.
     break
 
 
--- my_languages/youtubemusic_download_mp3.py
+++ my_languages/youtubemusic_download_mp3.py
@@ -1,8 +1,11 @@
 import youtube_dl
 def download_ytvid_as_mp3():
-    video_url = input("enter url of youtube video:")
+    video_code = ""
+    if 
+    video_url = "https://www.youtube.com/watch?v=" + video_code
     video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
-    filename = f"my_languages\\music_resources\\{video_info['title']}.mp3"
+    music_title = video_info['title'].replace(" ", "")
+    filename = f"my_languages\\music_resources\\{music_title}.mp3"
     options={
         'format':'bestaudio/best',
         'keepvideo':False,
@@ -11,5 +14,3 @@ def download_ytvid_as_mp3():
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
-
-    print("다운로드가 완료되었습니다... {}".format(filename))
